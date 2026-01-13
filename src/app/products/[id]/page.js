9f0f7388f619b656b8f95f229dedb821545c535d
SiddheshSoni <work.siddheshsoni@gmail.com>
2026-01-13 16:06:12 +0530

    handling static files

--- src/app/products/[id]/page.js
+++ src/app/products/[id]/page.js
@@ -1,19 +1,33 @@
-const getProduct = async (id) =>{
-    const res = await fetch(`http://localhost:3000/api/products/${id}`, { next:{revalidate: 60}});
-    return res.json();
-}
+import Image from "next/image";
 
-export default async function Product ({params}){
-    const { id } = await params;
-    const product = await getProduct(id);
-
-    return(
-        <div className="p-10">
-            <h1>{`Product ${product.id} details page`}</h1>
-            <p>{product.title}</p>
-            <p>₹{product.price}</p>
-            <p>{product.category}</p>
-            <p>{product.description}</p>
-        </div>
-    );
+const getProduct = async (id) => {
+  const res = await fetch(`http://localhost:3000/api/products/${id}`, {
+    next: { revalidate: 60 },
+  });
+  return res.json();
 };
+
+export default async function Product({ params }) {
+  const { id } = await params;
+  const product = await getProduct(id);
+
+  return (
+    <div className="p-10">
+      <h1>{`Product ${product.id} details page`}</h1>
+      <div>
+        <p>{product.title}</p>
+        <p>₹{product.price}</p>
+        <p>{product.category}</p>
+        <p>{product.description}</p>
+      </div>
+      <div>
+        <Image
+          src="/assets/post.jpg"
+          alt="product image"
+          width={300}
+          height={300}
+        />
+      </div>
+    </div>
+  );
+}
