19cf7407d71939c97578960c2dc97ec07980ddbd
SiddheshSoni <work.siddheshsoni@gmail.com>
2026-01-15 13:22:49 +0530

    debugged 3

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,14 +1,16 @@
-
 import ProductsList from "../_UI/ProductList";
 
 export const metadata = {
-  title: "Products Store - Products List"
+  title: "Products Store - Products List",
 };
 
 export default async function ProductPage() {
-  const res = await fetch("/api/products", {
-    next: { revalidate: 60 }
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
+  const res = await fetch(`${baseUrl}/api/products`, {
+    next: { revalidate: 60 },
   });
+
   const data = await res.json();
 
   return (
