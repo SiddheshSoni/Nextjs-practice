7860ce1cbd14d5c45e0317b49715d7895c214f16
SiddheshSoni <work.siddheshsoni@gmail.com>
2026-01-15 13:18:57 +0530

    debugged 2

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  publicRuntimeConfig: {
+    baseUrl: process.env.NEXT_PUBLIC_BASE_URL,
+  },
   reactCompiler: true,
 };
 
